3890ac64fd32b469425a673986958ed851831cbe
forgeronvirtuel <forgeronvirtuel@gmail.com>
2022-10-18 11:35:41 +0200

    dev(key-handler): removing useless test

--- src/cache/key_index_test.go
+++ src/cache/key_index_test.go
@@ -6,45 +6,6 @@ import (
 	"testing"
 )
 
-func TestNewRoot(t *testing.T) {
-	r := NewRoot("/")
-	if !reflect.DeepEqual(&Node{name: "/"}, r) {
-		t.Fatal("Node the same struct")
-	}
-}
-
-func TestNode_Root(t *testing.T) {
-	t.Run("Simple case", func(t *testing.T) {
-		var n *Node
-		n = n.Root("/a/b/")
-		if n.name != "a" {
-			t.Fatal("Not equal to a")
-		}
-		if n.childs[0].name != "b" {
-			t.Fatal("Not equal to b")
-		}
-	})
-
-	t.Run("add with the same root", func(t *testing.T) {
-		var n = NewRoot("/")
-		n = n.Root("/a/c/")
-		n = n.Root("/a/b/")
-
-		if n.name != "/" {
-			t.Fatal("Not equal to /")
-		}
-		if n.childs[0].name != "a" {
-			t.Fatal("Not equal to a")
-		}
-		if n.childs[0].childs[0].name != "b" {
-			t.Fatal("Not equal to b")
-		}
-		if n.childs[0].childs[1].name != "c" {
-			t.Fatal("Not equal to c")
-		}
-	})
-}
-
 func TestRoot_Root(t *testing.T) {
 	var r *Root
 	r = r.Root("/a/b/c").
